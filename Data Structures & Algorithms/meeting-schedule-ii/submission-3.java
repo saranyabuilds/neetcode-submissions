7eccbffe75764d5ab4913494c727b28d286e58ff
Saranya <mailmesaranya@gmail.com>
2026-06-04 12:17:22 -0700

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.java
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+
+        int minRooms = intervals.size();
+        Collections.sort(intervals, Comparator.comparingInt(i->i.start));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for(Interval interval : intervals){
+
+            if(!minHeap.isEmpty() && minHeap.peek() <= interval.start)
+            {
+                minHeap.poll();
+            }
+
+            minHeap.offer(interval.end);
+
+
+        }
+        return minHeap.size();
+
+    }
+}
