043611391a3c3284b9fd8ddab2cebb3e9c40a0f8
Saranya <mailmesaranya@gmail.com>
2026-06-11 12:21:09 -0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.java
@@ -0,0 +1,32 @@
+class WordDictionary {
+    List<String> store;
+    public WordDictionary() {
+        store = new ArrayList<>();
+    }
+
+    public void addWord(String word) {
+        store.add(word);
+    }
+
+    public boolean search(String word) {
+        for (String w : store) {
+            // check iflength matches
+            if (w.length() != word.length()) {
+                continue;
+            }
+            // if it matches iterate each
+            int i = 0;
+            while (i < w.length()) {
+                if (w.charAt(i) == word.charAt(i) || word.charAt(i) == '.') {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            if( i == w.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+}
