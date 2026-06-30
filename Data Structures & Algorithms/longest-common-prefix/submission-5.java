62cd8ac286a3ead6122b395f3c37ffa1380799af
Saranya <mailmesaranya@gmail.com>
2026-06-29 18:01:43 -0700

    Add: longest-common-prefix - submission-5

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-5.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder res = new StringBuilder();
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            boolean isPrefix = false;
+            char curr = strs[0].charAt(i);
+            System.out.println("curr-" + curr);
+
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() > 0 && i < strs[j].length()) {
+                    if (curr == strs[j].charAt(i)) {
+                        if (j == strs.length - 1) {
+                            System.out.println("Add to prefix");
+                            res.append(curr);
+                        }
+                        continue;
+                    } else {
+                        return res.toString();
+                    }
+                } else {
+                    return res.toString();
+                }
+            }
+        }
+        return res.toString();
+    }
+}
